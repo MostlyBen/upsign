ba9f10bf3a5e2274b1da88a81f73f0d1fff6a9b2
MostlyBen <BenTheHoff@gmail.com>
2021-09-03 17:09:39 -0400

    Added links to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,46 @@
-import { SignOut } from ".."
+import { Link } from "react-router-dom";
+import { SignOut } from "../"
+import { withRouter } from "react-router";
 
-const NavBar = (props) => {
+import logo from '../../img/logo192.png'
+
+const TeacherLinks = (props) => {
+  return (
+    <div>
+      <Link
+        to="/"
+        className="navbar-link"
+      >
+        <img
+          src={logo}
+          height="40"
+          alt="logo"
+          style={{
+            paddingRight: "1.5rem",
+            position: "relative",
+            verticalAlign: "middle",
+            top: "-4px"
+          }}
+        />
+        Home
+      </Link>
+
+      <Link
+        to="/overview"
+        className="navbar-link"
+      >
+        All Sessions
+      </Link>
+    </div>
+  )
+}
+
+const NavBar = ({ userType, match }) => {
   return (
     <div className="navbar-fixed">
       <nav>
         <div className="nav-wrapper white valign-wrapper">
+          {userType === "teacher" ? <TeacherLinks match={match} /> : <div />}
           <SignOut style={{position: "absolute", right: "2rem"}} />
         </div>
       </nav>
@@ -12,4 +48,4 @@ const NavBar = (props) => {
   )
 }
 
-export default NavBar
+export default withRouter(NavBar)
