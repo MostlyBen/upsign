47c335805d3897c882ee208406906ef437e06c14
MostlyBen <BenTheHoff@gmail.com>
2021-09-04 10:16:59 -0400

    Indexed session overview

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
+// Generic Page Components
 import NavBar from './NavBar/NavBar'
-
+// User Data Related
 import UserTypeSelect from './auth/UserTypeSelect'
 import SignOut from './auth/SignOut'
-
+// Main SignUp Pages
 import StudentSignUp from './SignUp/StudentSignUp'
 import TeacherSignUp from './SignUp/TeacherSignUp'
+// Dashboards
+import AllSessionOverview from './Dashboard/AllSessionOverview'
 
 export {
   NavBar,
@@ -14,4 +17,6 @@ export {
   
   StudentSignUp,
   TeacherSignUp,
+
+  AllSessionOverview,
 }
