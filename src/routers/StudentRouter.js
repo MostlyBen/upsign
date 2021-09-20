4f270596cda71a2e7572de8c80d53822407285f1
MostlyBen <BenTheHoff@gmail.com>
2021-09-20 17:49:51 -0400

    Removed test route

--- src/routers/StudentRouter.js
+++ src/routers/StudentRouter.js
@@ -2,21 +2,15 @@ import { Route, Switch } from 'react-router-dom';
 
 import { StudentSignUp } from '../components';
 
-const TestComponent = () => {
-  return (
-    <div>Test Component</div>
-  )
-}
 
-const TeacherRouter = ({ db, user }) => {
+const StudentRouter = ({ db, user }) => {
 
   return (
       <Switch>
-        <Route path="/test" render={() => ( <TestComponent /> )} />
         
         <Route path="/" render={() => ( <StudentSignUp db={db} user={user} /> )} />
       </Switch>
   )
 }
 
-export default TeacherRouter
+export default StudentRouter
