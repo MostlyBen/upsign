2b8eacba2a2f70184393d6932ecb13dc3ff8e7ce
MostlyBen <benthehoff@gmail.com>
2021-11-25 20:28:59 -0600

    Fixed getGroups

--- src/utils/groups/getGroups.js
+++ src/utils/groups/getGroups.js
@@ -1,20 +1,19 @@
 import { doc, getDoc } from "@firebase/firestore"
 
-  const getGroups = async (db) => {
-    const groupRef = doc(db, "config", "student_groups")
-    getDoc(groupRef)
-      .then(groupSnap => {
-        if (groupSnap.exists()) {
-          const groupList = groupSnap.data().groups
+const getGroups = async (db) => {
+  const groupRef = doc(db, "config", "student_groups")
+  const groupSnap = await getDoc(groupRef)
 
-          if (Array.isArray(groupList)) {
-            // setGroupOptions(groupList)
-            return groupList
-          } else {
-            return []
-          }
-        }
-      })
+  if (groupSnap.exists()) {
+    const groupList = groupSnap.data().groups
+    if (Array.isArray(groupList)) {
+      return groupList
+    } else {
+      return []
+    }
+  } else {
+    return []
   }
+}
 
 export default getGroups
