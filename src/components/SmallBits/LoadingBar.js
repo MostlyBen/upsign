ee485a5096860f76ffeebfe1ff2d61ae61ea3fa1
MostlyBen <benthehoff@gmail.com>
2022-04-20 16:39:53 -0400

    Updated loading bar to not cover what's above it

--- src/components/SmallBits/LoadingBar.js
+++ src/components/SmallBits/LoadingBar.js
@@ -1,11 +1,7 @@
 const LoadingBar = () => {
   return (
     <div className="progress grey lighten-5" style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      height: '100%',
-      width: '100%',
+      height: '100vh',
     }}>
         <div className="indeterminate grey lighten-3"></div>
     </div>
