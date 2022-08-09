237f13121b01ed83fa976371784a0c51fcc8c55a
MostlyBen <BenTheHoff@gmail.com>
2022-08-09 19:31:33 -0400

    Fixed host name getting

--- src/utils/info/getSubdomain.js
+++ src/utils/info/getSubdomain.js
@@ -6,16 +6,19 @@ const getSubdomain = () => {
     const arr = host
     // Regex to select periods and colons
     .split(/[.:]/)
+
     var i = arr.indexOf("localhost")
+    
     return i > 0 ? arr.slice(0, i)[0] : 'museum'
   
   // Make this work online
   } else if ( host.includes("upsign") ) {
     const arr = host
-    .split(".")
-    .splice(0, host.includes("upsign") ? -1 : -2);
+    .split(/[.:]/)
 
-    return i > 0 ? arr.slice(0, i) : 'museum'
+    var j = arr.indexOf("upsign")
+    
+    return j > 0 ? arr.slice(0, j)[0] : 'museum'
 
   } else {
     return null
