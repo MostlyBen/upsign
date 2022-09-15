94bb35ac39bb1d8e63dd8c13aec5076493dcf439
MostlyBen <benthehoff@gmail.com>
2022-09-15 12:37:11 -0400

    Fixed nicknames not making it to enrollments

--- src/services/enrollment/enrollStudent.js
+++ src/services/enrollment/enrollStudent.js
@@ -24,6 +24,7 @@ const enrollStudent = async (db, date, session, user) => {
   const payload = {
     attendance: '',
     name: user.name,
+    nickname: user.nickname,
     session: Number(session.session),
     session_id: session.id,
     teacher_id: session.teacher_id,
