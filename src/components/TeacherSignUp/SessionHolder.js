d1a7c79ce35d7b193ac2dd1710b870d976ee920e
MostlyBen <benthehoff@gmail.com>
2023-09-19 14:11:23 -0400

    Fixed multi-session hour formatting for teachers

--- src/components/TeacherSignUp/SessionHolder.js
+++ src/components/TeacherSignUp/SessionHolder.js
@@ -7,11 +7,12 @@ const SessionHolder = ({ db, selectedDate, hour, sessionTimes, sessions }) => {
       <span className="session-time"> {sessionTimes[hour - 1] ? '('+sessionTimes[hour - 1]+')': ''}</span>
     </h4>
     <hr style={{marginBottom: "1rem"}} />
-    <div className="row card session-card is-enrolled teacher-card">
       {sessions.map(s => {
-        return <SessionEditor key={s.id} session={s} db={db} date={selectedDate} />
-      })}
-    </div>
+        return (
+          <div key={`teacher-card-${s.id}`} className="row card session-card is-enrolled teacher-card">
+            <SessionEditor session={s} db={db} date={selectedDate} />
+          </div>
+        )})}
   </div>
   )
 
