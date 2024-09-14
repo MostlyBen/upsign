0840b3384599a34b616a5d074b58b944803a380b
MostlyBen <benthehoff@gmail.com>
2024-09-14 19:12:51 -0400

    Slightly more room on mobile for students

--- app/layouts/StudentLayout.tsx
+++ app/layouts/StudentLayout.tsx
@@ -5,7 +5,7 @@ export default function StudentLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <NavBar userType="student" schoolName="" />
-      <div className="bg-base-200 min-h-screen p-12 md:py-12 md:px-16 lg:px-24 xl:px-32 2xl:px-64">
+      <div className="bg-base-200 min-h-screen p-6 md:py-12 md:px-16 lg:px-24 xl:px-32 2xl:px-64">
         <div className="mt-16">
           {children}
         </div>
