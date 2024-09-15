8ec77b508a2adad70a07500b65b1bb23c1078662
MostlyBen <benthehoff@gmail.com>
2024-09-15 15:28:58 -0400

    Updated spacing

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth, onAuthStateChanged} from 'firebase/auth';
+import { getAuth, Auth, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, Firestore } from "firebase/firestore";
 import { useState, useEffect } from 'react';
 
@@ -9,14 +9,14 @@ let firestore: Firestore;
 
 if (typeof window !== "undefined") {
   firebaseApp = initializeApp({
-      apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-      authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-      databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
-      projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-      storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-      messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-      appId: import.meta.env.VITE_FIREBASE_APPID,
-    });
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
+  });
 
   auth = getAuth(firebaseApp);
   firestore = getFirestore(firebaseApp);
