f896209c3d06e2b81079ac4744fb80547e682c19
MostlyBen <benthehoff@gmail.com>
2024-09-15 17:37:16 -0400

    More feedback on what group users are being added to / removed from

--- app/components/Config/Groups.tsx
+++ app/components/Config/Groups.tsx
@@ -145,7 +145,7 @@ const StudentGroups = ({ db }: StudentGroupsProps) => {
       {/* Student List */}
       <div className="grid grid-cols-2 gap-2" style={{ maxHeight: "calc(100dvh - 36rem)", overflowY: "auto" }}>
         <div>
-          <h2 className="mt-0 mb-2 text-center">Not in Group</h2>
+          <h2 className="mt-0 mb-2 text-center">Not in {selectedGroup}</h2>
           {filteredStudents.filter(s => !s.groups?.includes(selectedGroup)).map(student => {
             return (
               <StudentName
@@ -158,7 +158,7 @@ const StudentGroups = ({ db }: StudentGroupsProps) => {
           })}
         </div>
         <div>
-          <h2 className="mt-0 mb-2 text-center">In Group</h2>
+          <h2 className="mt-0 mb-2 text-center">In {selectedGroup}</h2>
           {filteredStudents.filter(s => s.groups?.includes(selectedGroup)).map(student => {
             return (
               <StudentName
