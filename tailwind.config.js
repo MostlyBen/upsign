410ccea929920c4b8a191236430f6a5bf45b329f
Sapphire <116238485+Ouraniasapphire@users.noreply.github.com>
2024-12-18 21:03:59 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,6 +78,15 @@ module.exports = {
       },
 
       {
+        sapphiredark: {
+          ...require("daisyui/src/theming/themes").night,
+          "--present": "#3EB2FF",
+          "--tardy": "#ab830d",
+          "--absent": "#BC0000"
+        }
+      },
+
+      {
         thursday: {
           ...require("daisyui/src/theming/themes").autumn,
           "--present": "#157A6E",
@@ -109,14 +118,6 @@ module.exports = {
         }
       },
       
-      {
-        sapphiredark: {
-          ...require("daisyui/src/theming/themes").night,
-          "--present": "#3EB2FF",
-          "--tardy": "#ab830d",
-          "--absent": "#BC0000"
-        }
-      },
     ],
   }
 }
