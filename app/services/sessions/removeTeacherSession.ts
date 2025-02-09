9f2bfbd9c212ba28c074364482b75a83e9e0dc3a
MostlyBen <benthehoff@gmail.com>
2025-02-09 18:20:54 -0500

    Remove enrollments when session is deleted

--- app/services/sessions/removeTeacherSession.ts
+++ app/services/sessions/removeTeacherSession.ts
@@ -1,18 +1,43 @@
-import { doc, deleteDoc, Firestore } from "@firebase/firestore";
+import { doc, deleteDoc, Firestore, runTransaction, query, where, getDocs, collection } from "@firebase/firestore";
 import { getSchoolId } from "../../utils";
 
-const removeTeacherSession = async (db: Firestore, date: Date, sessionId: string, schoolId: string | null = null): Promise<void> => {
+const removeTeacherSession = async (
+  db: Firestore,
+  date: Date,
+  sessionId: string,
+  schoolId: string | null = null
+): Promise<void> => {
   if (schoolId === null) {
     schoolId = getSchoolId();
   }
-  
+
   const sessionRef = doc(
     db,
     `schools/${schoolId}/sessions/${date.getFullYear()}/${date.toDateString()}/${sessionId}`
   );
 
-  // Remove the doc
-  await deleteDoc(sessionRef);
+  const enrollmentsRef = collection(
+    db,
+    `schools/${schoolId}/sessions/${date.getFullYear()}/${date.toDateString()}-enrollments`
+  );
+
+  // Get all enrollments for this session
+  const q = query(enrollmentsRef,
+    where("session_id", "==", sessionId)
+  );
+
+  const qSnapshot = await getDocs(q);
+
+  // Delete all enrollments and the session
+  await runTransaction(db, async (transaction: any) => {
+    qSnapshot.forEach(async (snap) => {
+      const enrollment = snap.data();
+      transaction.delete(snap.ref);
+      transaction.delete(doc(enrollmentsRef, enrollment.uid));
+    });
+    transaction.delete(sessionRef);
+  });
+
 }
 
 export default removeTeacherSession;
