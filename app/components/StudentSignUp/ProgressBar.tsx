556af494a195998eec852062d7b6ddc15a445155
MostlyBen <benthehoff@gmail.com>
2025-09-25 14:14:14 -0400

    Animate progress bar in

--- app/components/StudentSignUp/ProgressBar.tsx
+++ app/components/StudentSignUp/ProgressBar.tsx
@@ -1,12 +1,12 @@
 export default function ProgressBar({ percent }: { percent: number }) {
   return (
     <>
-      <progress
+      {percent > 0 && <progress
         className="progress progress-primary signup-progress"
         value={percent}
         style={{ borderRadius: "0 !important" }}
         max="1"
-      />
+      />}
     </>
   )
 }
